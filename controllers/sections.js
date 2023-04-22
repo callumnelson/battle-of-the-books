952c234680da716b547a0f587f065ce6f795a574
Callum Nelson <callum.nelson95@gmail.com>
2023-04-22 13:34:10 -0400

    update section index controller and view

--- controllers/sections.js
+++ controllers/sections.js
@@ -3,12 +3,28 @@ import { Profile } from '../models/profile.js'
 
 const index = async (req, res) => {
   try {
-    const teacher = await Profile.findById(req.user.profile._id)
-    .populate('sections')
-    res.render('sections/index', { 
-      title: 'Sections Home',
-      teacher 
-    })
+    if (req.user.profile.role > 100) {
+      const teacher = await Profile.findById(req.user.profile._id)
+      .populate('sections')
+      res.render('sections/index', { 
+        title: 'Sections',
+        teacher
+      })
+    // A student is trying to load their sections
+    } else {
+      const student = await Profile.findById(req.user.profile._id)
+      .populate('sections')
+      //If they're enrolled in at least one section, show what they're enrolled in
+      if (student.sections.length) {
+        res.render('sections/index', {
+          title: 'Sections',
+          student
+        })
+      } else {
+        //TODO res.render message to ask a teacher for their signup code..?
+        res.redirect('/')
+      }
+    }
   } catch (err) {
     console.log(err)
     res.redirect('/')
@@ -27,9 +43,10 @@ const create = async (req, res) => {
       //TODO make this show to the user
       throw new Error('Oops, duplicate section!')
     } else {
-      req.body.teacher = req.user.profile._id
       const newSection = await Section.create(req.body)
+      newSection.teachers.push(teacher._id)
       teacher.sections.push(newSection._id)
+      await newSection.save()
       await teacher.save()
       res.redirect('/sections')
     }
@@ -41,17 +58,20 @@ const create = async (req, res) => {
 
 const show = async (req, res) => {
   try {
-    const section = await Section.findById(req.params.sectionId)
-    .populate('teachers')
-    //Make sure that this teacher is actually the owner of the section
-    if (section.teachers.includes(req.user.profile._id)){
-      res.render('sections/show', {
-        title: section.name,
-        section
-      })
-    }else {
-      throw new Error(`That's not your section!`)
-    }
+    //If a teacher is trying to access the sections show page
+    if (req.user.profile.role > 100){
+      const section = await Section.findById(req.params.sectionId)
+      console.log(section)
+      //Make sure that this teacher is actually the owner of the section
+      if (section.teachers.includes(req.user.profile._id)){
+        res.render('sections/show', {
+          title: section.name,
+          section
+        })
+      } else {
+        throw new Error(`That's not your section!`)
+      }
+    } 
   } catch (err) {
     console.log(err)
     res.redirect('/sections')
