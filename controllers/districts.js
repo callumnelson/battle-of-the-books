b1801c43aa162c59b789023b3a56a7432cfbd724
Callum Nelson <callum.nelson95@gmail.com>
2023-04-26 20:20:19 -0400

    implement full school and district deletion

--- controllers/districts.js
+++ controllers/districts.js
@@ -1,6 +1,8 @@
 import { District } from '../models/district.js'
 import { Profile } from '../models/profile.js'
+import { Section } from '../models/section.js'
 import { User } from '../models/user.js'
+import { Ticket } from '../models/ticket.js'
 
 const index = async (req, res) => {
   try {
@@ -60,7 +62,12 @@ const deleteDistrict = async (req, res) => {
       const toDelete = await District.findById(req.params.districtId)
       const districtProfiles = await Profile.find({district: req.params.districtId})
       const districtEmails = districtProfiles.map(prof => prof.email)
-      await Profile.deleteMany({district: toDelete._id})
+      const districtSections = districtProfiles.reduce( (a, c) => [...a, ...c.sections], [])
+      const districtTickets = districtProfiles.reduce( (a, c) => [...a, ...c.tickets], [])
+      
+      await Section.deleteMany({_id: { $in: districtSections}})
+      await Ticket.deleteMany({_id: { $in: districtTickets}})
+      await Profile.deleteMany({district: req.params.districtId})
       await User.deleteMany({email: {$in: districtEmails}})
       await toDelete.deleteOne()
       res.redirect('/districts')
@@ -77,14 +84,25 @@ const deleteSchool = async (req, res) => {
   try {
     //Only admin can take this action
     if (req.user.profile.role > 200){
-      console.log(req.body)
-      // const toDeleteDistrict = await District.find(req.params.districtId)
-      // const schoolProfiles = await Profile.find({district: req.params.districtId})
-      // const districtEmails = districtProfiles.map(prof => prof.email)
-      // await Profile.deleteMany({_id: toDelete._id})
-      // await User.deleteMany({email: {$in: districtEmails}})
-      // await toDelete.deleteOne()
-      // res.redirect('/districts')
+      const district = await District.findById(req.params.districtId)
+      district.schools.remove(req.body.school)
+      await district.save()
+      const schoolProfiles = await Profile.find({
+        district: req.params.districtId,
+        school: req.body.school
+      })
+      const schoolEmails = schoolProfiles.map(prof => prof.email)
+      const schoolSections = schoolProfiles.reduce( (a, c) => [...a, ...c.sections], [])
+      const schoolTickets = schoolProfiles.reduce( (a, c) => [...a, ...c.tickets], [])
+      
+      await Section.deleteMany({_id: { $in: schoolSections}})
+      await Ticket.deleteMany({_id: { $in: schoolTickets}})
+      await Profile.deleteMany({
+        district: req.params.districtId,
+        school: req.body.school
+      })
+      await User.deleteMany({email: {$in: schoolEmails}})
+      res.redirect('/districts')
     }else {
       throw new Error(`Access Denied: Inadequate permissions to delete district`)
     }
